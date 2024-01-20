d4e95a2d30c44c0a5486bcf7ce42379564b27341
Nickker <mnn@rigintelpro.ru>
2024-01-20 20:14:01 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>       
+     <div className='anim_window'>
+        <div className='anim_window_l'>
+          <div className='anim_window_ll'>
+             <img className="rect_l" src="\img\rect_l.svg"/>
+          </div>
+        </div>
+        <div className='anim_window_r'>
+          <div className='anim_window_rr'>
+            <img className="rect_r" src="\img\rect_r.svg"/>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
