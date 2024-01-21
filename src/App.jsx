2d40da7ee4064e07e1761ecf012ad02839678ec6
Nickker <mnn@rigintelpro.ru>
2024-01-21 12:40:58 +0500

    del_extra_DIV

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,12 @@ function App() {
   return (
     <>       
      <div className='anim_window'>
-        <div className='anim_window_l'>
           <div className='anim_window_ll'>
              <img className="rect_l" src="\img\rect_l.svg"/>
-          </div>
-        </div>
-        <div className='anim_window_r'>
+         </div>
           <div className='anim_window_rr'>
             <img className="rect_r" src="\img\rect_r.svg"/>
-          </div>
-        </div>
+         </div>
       </div>
     </>
   )
